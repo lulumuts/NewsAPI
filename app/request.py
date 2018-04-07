6cd010a9302a8ad4b568b9f02118a7678cf9cdab
Lulu <lulumutuli24@gmail.com>
2018-04-07 09:07:38 +0300

    function created in request.py to get API data

--- app/request.py
+++ app/request.py
@@ -1,4 +1,30 @@
 from app import app
+import urllib.request, json
+from .models import news
+
+News=news.News
 
 #Getting api key
 api_key = app.config['NEWS_API_KEY']
+
+#Getting the movie base url
+base_url = app.config['NEWS_API_BASE_URL']
+
+def get_sources(category):
+    '''
+    Function that gets the json response to our url request
+    '''
+
+    get_sources_url = base_url.format(category,api_key)
+
+    with urllib.request.urlopen(get_sources_url) as url:
+        get_sources_data = url.read()
+        get_sources_response = json.loads(get_sources_data)
+
+        sources_results= None
+
+        if get_sources_response['results']:
+            sources_results_list = get_sources_response['results']
+            sources_results = process_results(sources_results_list)
+
+    return sources_results
