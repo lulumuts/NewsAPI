ed16a26221bf78ee879de0fe858dd9acb8bdc6a1
Lulu <lulumutuli24@gmail.com>
2018-04-09 09:59:14 +0300

    request.py updated to to change base_url and api_key to none and to define configure_request

--- app/request.py
+++ app/request.py
@@ -1,20 +1,27 @@
 from app import app
 import urllib.request, json
 from newsapi import NewsApiClient
-from .models import news, articles
+from .models import News, Articles
 
 News=news.News
 
 #Getting api key
-api_key = app.config['NEWS_API_KEY']
+api_key = None
+# api_key = app.config['NEWS_API_KEY']
 
 #Getting the movie base url
-base_url = app.config['NEWS_API_BASE_URL']
+base_url = None
+# base_url = app.config['NEWS_API_BASE_URL']
 
 #Getting the articles base url
-articles_base_url = app.config ['ARTICLES_API_BASE_URL']
+articles_base_url = None
+# articles_base_url = app.config ['ARTICLES_API_BASE_URL']
 
-
+def configure_request(app):
+    global api_key,base_url
+    api_key = app.config['NEWS_API_KEY']
+    base_url = app.config['NEWS_API_BASE_URL']
+    articles_base_url = app.config ['ARTICLES_API_BASE_URL']
 
 def get_sources(category):
     '''
